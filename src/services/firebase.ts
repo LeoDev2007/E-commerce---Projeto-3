9375ef4779cdade772d27a3a1921e34a6e8e3df9
LeoDev2007 <leonardodeveloper200@gmail.com>
2026-04-26 09:19:23 -0300

    refactor: update Firebase configuration to use environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,16 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkLjGqawxL12nYQYNzxdd6RvLEpvcF4sI",
-  authDomain: "shopgoauth.firebaseapp.com",
-  projectId: "shopgoauth",
-  storageBucket: "shopgoauth.firebasestorage.app",
-  messagingSenderId: "739375582621",
-  appId: "1:739375582621:web:c39cc107fe53a046da3374"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
